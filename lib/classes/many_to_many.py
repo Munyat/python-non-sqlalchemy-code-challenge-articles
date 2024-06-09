ae50674c38da0db44425daf23887b6419e671259
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-06-10 01:46:38 +0300

    comlete articles lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,139 @@
 class Article:
-    def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
-        
+    all = []
+
+    def __init__(self, author, magazine, title: str):
+        if not isinstance(author, Author):
+            raise TypeError("author must be an instance of Author")
+        if not isinstance(magazine, Magazine):
+            raise TypeError("magazine must be an instance of Magazine")
+        if not isinstance(title, str):
+            raise TypeError("Title must be of type str")
+        if not (5 <= len(title) <= 50):
+            raise ValueError("Title must be between 5 and 50 characters, inclusive")
+
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+
+        Article.all.append(self)
+        author._articles.append(self)
+        magazine._articles.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        # Prevent changing the title after instantiation
+        pass
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, new_author):
+        if not isinstance(new_author, Author):
+            raise TypeError("author must be an instance of Author")
+        self._author = new_author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if not isinstance(new_magazine, Magazine):
+            raise TypeError("magazine must be an instance of Magazine")
+        self._magazine = new_magazine
+
+
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str):
+            raise TypeError("Name must be of type str")
+        if len(name) == 0:
+            raise ValueError("Name must be longer than 0 characters")
+        self._name = name
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # Prevent changing the name after instantiation
+        pass
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list(set(article.magazine for article in self._articles))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        if not self._articles:
+            return None
+        return list(set(article.magazine.category for article in self._articles))
+
 
 class Magazine:
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self._articles = []
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and (2 <= len(value) <= 16):
+            self._name = value
+        # If the value is not a string or not within the valid length, ignore the assignment
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._category = value
+        # If the value is not a string or the length is zero, ignore the assignment
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self._articles))
 
     def article_titles(self):
-        pass
+        if not self._articles:
+            return None
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in self._articles:
+            if article.author in author_counts:
+                author_counts[article.author] += 1
+            else:
+                author_counts[article.author] = 1
+        contributing_authors = [author for author, count in author_counts.items() if count > 2]
+        return contributing_authors if contributing_authors else None
+
+    @classmethod
+    def top_publisher(cls):
+        if not cls.all:
+            return None
+        return max(cls.all, key=lambda magazine: len(magazine._articles))
